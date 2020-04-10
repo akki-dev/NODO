2e4abf3fdb83fcf42951bdb1c92987a0a42fc662
Akki Iride <akki_iride@outlook.com>
2020-04-09 23:15:07 -0500

    updatefinal

--- app.js
+++ app.js
@@ -3,18 +3,18 @@
 
    //REEMPLAZAR COMER CON LONCHE , AGREGAR ALMUERZO DESPUES DE REFRIGERIO Y ELMINAR SOBRA
    var
-   elementoUno = document.createElement('li'),
-   elementoDos = document.createElement('li'),
-   contenidoUno = document.createTextNode('Almuerzo'),
-   contenidoDos = document.createTextNode('Lonche'),
-   lista = document.getElementById('comida'),
-   sobra = document.getElementById('sobrante'),
-   comer = document.getElementById('Comer'),
-   elementoPadre = document.getElementsByTagName('li')[2].parentNode,
-   contenidoPadre = document.getElementsByTagName('li')[2];
+      elementoUno = document.createElement('li'),
+      elementoDos = document.createElement('li'),
+      contenidoUno = document.createTextNode('Almuerzo'),
+      contenidoDos = document.createTextNode('Lonche'),
+      lista = document.getElementById('comida'),
+      sobra = document.getElementById('sobrante'),
+      comer = document.getElementById('Comer'),
+      elementoPadre = document.getElementsByTagName('li')[2].parentNode,
+      contenidoPadre = document.getElementsByTagName('li')[2];
    elementoUno.appendChild(contenidoUno);
    elementoDos.appendChild(contenidoDos);
    lista.removeChild(sobra);
-   lista.replaceChild(elementoDos , comer);
+   lista.replaceChild(elementoDos, comer);
    elementoPadre.insertBefore(elementoUno, contenidoPadre);
 }())
